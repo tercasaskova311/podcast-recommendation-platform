a451173f168b9960e7a011a4d6578e59bed3f0eb
tercasaskova311 <tercasaskova311@gmail.com>
2025-06-07 09:32:18 +0200

    delete transcripts_foreign

--- spark/main.py
+++ spark/main.py
@@ -2,17 +2,15 @@ import argparse
 
 from spark.pipelines import (
     metadata_pipeline,
-    transcripts_en_pipeline,
-    transcripts_foreign_pipeline,
     streaming_pipeline,
-    summary_pipeline
+    summary_pipeline,
+    transcripts_pipeline
 )
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--job", choices=[
     "metadata",
-    "transcripts-en",
-    "transcripts-foreign",
+    "transcripts",
     "streaming",
     "summary"
 ], required=True)
@@ -20,10 +18,8 @@ args = parser.parse_args()
 
 if args.job == "metadata":
     metadata_pipeline.run_metadata_pipeline()
-elif args.job == "transcripts-en":
-    transcripts_en_pipeline.run_transcripts_en_pipeline()
-elif args.job == "transcripts-foreign":
-    transcripts_foreign_pipeline.run_transcripts_foreign_pipeline()
+elif args.job == "transcripts":
+    transcripts_pipeline.run_transcripts_en_pipeline()
 elif args.job == "streaming":
     streaming_pipeline.run_streaming_pipeline()
 elif args.job == "summary":
