c2647b500de9ddba28586a0fb016f78f8c3d8c53
Tommaso <tommy9798@hotmail.it>
2025-07-12 17:46:22 +0200

    Fix a bug

--- spark/jobs/transcripts_generator.py
+++ spark/jobs/transcripts_generator.py
@@ -1,4 +1,4 @@
-from pipelines.transcripts_pipeline import run_transcript_pipeline
+from spark.pipelines.download_transcripts_pipeline import run_transcript_pipeline
 
 if __name__ == "__main__":
     run_transcript_pipeline()
