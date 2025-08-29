aeea111b27e84892294ea8a93ec517801390ebf4
Tommaso <tommy9798@hotmail.it>
2025-08-29 09:03:18 +0200

    Schedule simulation script

--- airflow/dags/user_events.py
+++ airflow/dags/user_events.py
@@ -1,10 +1,14 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
+import yaml
+
+with open('/opt/airflow/config/schedule_config.yaml') as f:
+    config = yaml.safe_load(f)
 
 with DAG(
     dag_id="user_events",
-    schedule_interval=None,
+    schedule_interval=config['simulate_user_events_interval'],
     start_date = days_ago(1),
     catchup=False,
     tags=["user events", "simulation"],
